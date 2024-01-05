68927d80581d95060dafe143f41e73ab6e90bcf9
Harsh Solanki <harsh.solanki2105@gmail.com>
2024-01-05 15:45:17 +0530

    feat: Upgraded tests

--- tests/user.test.ts
+++ tests/user.test.ts
@@ -1,27 +1,40 @@
 import request from 'supertest'
 import app from '../src/server'
+import connectMongo from '../src/configs/mongo'
 
 describe('User Authentication', () => {
-    let token: string
+    const numUsers: number = 3
 
-    // Unit Test: Signup
-    test('should sign up a new user', async () => {
-        const response = await request(app).post('/api/auth/signup').send({
-            name: 'harsh',
-            email: 'harsh@gmail.com',
-            password: 'harsh123',
-            confirm_password: 'harsh123',
-        })
-
-        expect(response.status).toBe(200)
+    beforeAll(async () => {
+        await connectMongo(process.env.MONGO_URI as string)
     })
 
-    // Integration Test: Login
-    test('should log in an existing user', async () => {
-        const response = await request(app)
-            .post('/api/auth/login')
-            .send({ email: 'harsh@gmail.com', password: 'harsh123' })
+    test('should sign up a new user', async () => {
+        for (let i = 1; i <= numUsers; i++) {
+            const response = await request(app)
+                .post('/api/auth/signup')
+                .send({
+                    name: `harsh${i}`,
+                    email: `harsh_${i}@gmail.com`,
+                    password: `harsh_${i}`,
+                    confirm_password: `harsh_${i}`,
+                })
 
-        expect(response.status).toBe(200)
+            expect(response.status).toBe(201)
+            expect(response.body).toHaveProperty('user_id')
+            expect(response.body.message).toBe('User created Successfully')
+            // createdUserIds.push(response.body.user_id)
+        }
+    })
+
+    test('should log in an existing user', async () => {
+        for (let i = 1; i <= numUsers; i++) {
+            const response = await request(app)
+                .post('/api/auth/login')
+                .send({ email: `harsh_${i}@gmail.com`, password: `harsh_${i}` })
+
+            expect(response.status).toBe(200)
+            expect(response.body.message).toBe('LoggedIn Succesfully')
+        }
     })
 })
